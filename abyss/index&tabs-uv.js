c41492fdda8286eae89a58251972332662fcc3af
0xkkl <kaydan@leozc.com>
2023-03-19 20:12:25 -0700

    fixed# with '#' will be ignored, and an empty message aborts the commit.

--- abyss/index&tabs-uv.js
+++ abyss/index&tabs-uv.js
@@ -27,7 +27,7 @@ function go(value) {
       else if (!(url.startsWith("https://") || url.startsWith("http://")))
         url = "https://" + url;
       iframe.style.display = "block"
-      iframe.src = "/service/" + __uv$config.encodeUrl(url);
+      iframe.src = __uv$config.prefix + __uv$config.encodeUrl(url);
       //var iframeurl = __uv$config.decodeUrl(iframe.src)
       var iframeurl = iframe.src.substring(iframe.src.indexOf("/service/") + 9);
       //document.querySelector("#urlbartop input").value = iframeurl.substring(iframeurl.indexOf("/service/") + 0);
